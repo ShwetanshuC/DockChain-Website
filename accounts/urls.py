d5bcd1dadd56be36bde61439b0069f4da6ae6885
Shwetanshu <shwetanshu.c@gmail.com>
2025-07-04 22:17:03 -0400

    Sign up
    Sign up page made

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import SignUpView
 
 urlpatterns = [
     path("success/", views.success, name='success'),
+    path("signup/", SignUpView.as_view(), name="signup"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,3 +2,14 @@ from django.shortcuts import render, HttpResponse
 
 def success(request): 
     return render(request, "success.html")
+
+#sign up page
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
