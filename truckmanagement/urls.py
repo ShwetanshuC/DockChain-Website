5bb6aa40cda94b2600cc565c039a732efac6b650
Shwetanshu <shwetanshu.c@gmail.com>
2025-07-07 10:50:46 -0400

    Fully working trucker cards

--- truckmanagement/urls.py
+++ truckmanagement/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.truckmanagement, name='truckmanagement'),
     path("driverdirectory/", views.driverdirectory, name='driverdirectory'),
+    path('delete/<int:id>/', views.delete_trucker, name='delete_trucker'),
 ]
--- truckmanagement/views.py
+++ truckmanagement/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import trucker
 from .forms import TruckerForm
+from django.shortcuts import get_object_or_404
 
 def trucker_list(request):
     truckers = trucker.objects.all().order_by('-date_posted')
@@ -33,11 +34,33 @@ def driverdirectory(request):
     truckers = trucker.objects.all().order_by('-date_posted')
     
     if request.method == 'POST':
-        form = TruckerForm(request.POST)
-        if form.is_valid():
-            form.save()
+        print("POST ACTION:", request.POST.get('action'))
+        print("EDIT ID:", request.POST.get('edit_id'))
+        action = request.POST.get('action')
+
+        if action == 'edit':
+            edit_id = request.POST.get('edit_id')
+            t = get_object_or_404(trucker, id=edit_id)
+            t.firstname = request.POST.get('firstname')
+            t.lastname = request.POST.get('lastname')
+            t.role = request.POST.get('role')
+            t.save()
             return redirect('driverdirectory')
+
+        elif action == 'add':
+            firstname = request.POST.get('firstname')
+            lastname = request.POST.get('lastname')
+            role = request.POST.get('role')
+            trucker.objects.create(firstname=firstname, lastname=lastname, role=role)
+            return redirect('driverdirectory')
+
     else:
         form = TruckerForm()
 
-    return render(request, "driverdirectory.html", {'truckers': truckers, 'form': form})
+    return render(request, "driverdirectory.html", {'truckers': truckers, 'form': TruckerForm()})
+
+def delete_trucker(request, id):
+    if request.method == 'POST':
+        trucker_to_delete = get_object_or_404(trucker, id=id)
+        trucker_to_delete.delete()
+    return redirect('driverdirectory')
